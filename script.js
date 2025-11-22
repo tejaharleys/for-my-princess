58db6cc6655ed7f3166d93ba1b4ca6d992239095
Thejeshwaar paasila <thejeshwaarp@gmail.com>
2025-11-22 19:47:11 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function moveRandomEl(elm) {
+    if (!elm) return;
     elm.style.position = "absolute";
     elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
     elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
-  }
-  
-  const moveRandom = document.querySelector("#move-random");
-  
-  moveRandom.addEventListener("mouseenter", function (e) {
-    moveRandomEl(e.target);
-  });
+}
+
+const moveRandom = document.querySelector("#move-random");
+
+if (moveRandom) {
+    moveRandom.addEventListener("mouseenter", function (e) {
+        moveRandomEl(e.target);
+    });
+}
